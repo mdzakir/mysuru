8b8cd8aed978f42b9eb85c0ab6aa5b5a2f44b4bd
zakirmd <muhammad.zakir@axisrooms.com>
2017-02-14 00:38:54 +0530

    urls change

--- mysore/settings.py
+++ mysore/settings.py
@@ -34,6 +34,11 @@ connect('mysore_local', host='mongo')
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:8081/',
+    'hostname.example.com'
+)
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -67,6 +72,13 @@ MIDDLEWARE_CLASSES = [
 
 ]
 CORS_ORIGIN_ALLOW_ALL = True
+
+
+CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:8081/',
+    'hostname.example.com'
+)
+
 ROOT_URLCONF = 'mysore.urls'
 
 TEMPLATES = [
